7376ea245cbba99c999118ef1c1de53cc17e30ad
thamil <thamil@gmail.com>
2025-02-03 10:15:24 +0530

    first commit

---
+++ router/ProductRoutes.js
@@ -0,0 +1,9 @@
+const productController = require('../controller/ProductController')
+const router = require('express').Router()
+
+router.get('/api/products', productController.getAllProducts);
+router.get('/api/products/:id', productController.getSingleProduct)
+router.post('/api/product', productController.addnewProduct)
+router.put('/api/product', productController.updateProduct)
+router.delete('/api/product/:id', productController.deleteProduct)
+module.exports = router
